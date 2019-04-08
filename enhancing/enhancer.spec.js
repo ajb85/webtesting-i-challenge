e09df120c8d111b5f14fb204ee6d89b19fecbd04
ajb85 <ajbrush85@gmail.com>
2019-04-08 15:15:34 -0400

    Initial commit.  Added jest, added descriptions to spec.js

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,12 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+description("enchancer.js", () => {
+  description("succeed", () => {});
+
+  description("fail", () => {});
+
+  description("repair", () => {});
+
+  description("get", () => {});
+});
